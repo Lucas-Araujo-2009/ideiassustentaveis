9c450f89f91bb8130efed506c049fc15477479c3
Lucas <166239524+Lucas-Araujo-2009@users.noreply.github.com>
2024-11-27 21:22:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-console.log("Pô, mano to cansadão.");
+function IrParaPaginaAgua() {
+    window.location.href = "ideia-economize-agua.html";
+}
+
+function IrParaPaginaPlastico(){
+  window.location.href = "ideia-reduzir-plasticos.html";
+}
+
+function IrParaPaginaCompostagem(){
+  window.location.href = "ideia-compostagem.html";
+}
+
+function IrParaPaginaConsumo(){
+  window.location.href = "ideia-consumo-consciente.html";
+}
+
+function IrParaPaginaEnergia(){
+  window.location.href = "ideia-energia-solar.html";
+}
+
+function IrParaPaginaTransporte(){
+  window.location.href = "ideia-transporte.html";
+}
+
+function IrParaPaginaResiduos(){
+  window.location.href = "ideia-separar-residuos.html";
+}
+
+function IrParaPaginaArquitetura(){
+  window.location.href = "ideia-arquitetura.html";
+}
+
+function IrParaPaginaVerde(){
+  window.location.href = "ideia-mais-verde.html";
+}
